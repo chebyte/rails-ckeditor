dcf5c7cb3a86e500834dfe248e642ed983cb0a08
chebyte <maurotorres@gmail.com>
2011-06-14 22:14:25 -0300

    fixing attachment plugin and adding spanish support

---
+++ generators/ckeditor_install/templates/ckeditor/plugins/attachment/lang/es.js
@@ -0,0 +1,10 @@
+CKEDITOR.plugins.setLang('attachment', 'es',
+{
+  attachment : 
+  {
+    title : "Insertar Archivo",
+    url: "URL",
+    name: "Titulo",
+    button : "Insertar archivo"
+  }
+});
